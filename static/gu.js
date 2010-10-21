968c1ea8ab4a40f070bbfd062a48b73828e6f41d
Jason Johnson <jason@period-three.com>
2010-10-20 20:57:21 -0500

    added headline and trailtext tests

--- static/gu.js
+++ static/gu.js
@@ -1,5 +1,5 @@
 var api_key = 'tnug2qhxwphygdm5d7g99wb6';
-var api_url = 'http://content.guardianapis.com/news?format=json&api-key='+api_key+'&callback=?';
+var api_url = 'http://content.guardianapis.com/news?format=json&show-fields=all&api-key='+api_key+'&callback=?';
 
 var get_summaries_for_section = function(section, callback) {
     $.getJSON(api_url, function(data) {

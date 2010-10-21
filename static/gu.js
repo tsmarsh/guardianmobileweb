e5b23a6976b23f888183b2d1c566d8f9c229fe79
Jason Johnson <jason@period-three.com>
2010-10-20 20:47:21 -0500

    beginnings of qunit usage

---
+++ static/gu.js
@@ -0,0 +1,8 @@
+var api_key = 'tnug2qhxwphygdm5d7g99wb6';
+var api_url = 'http://content.guardianapis.com/news?format=json&api-key='+api_key+'&callback=?';
+
+var get_summaries_for_section = function(section, callback) {
+    $.getJSON(api_url, function(data) {
+        callback(data.response.results);
+    });
+}
